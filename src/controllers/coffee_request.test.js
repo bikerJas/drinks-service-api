ab5154b143d379f2a5288f2157aafc7ec0daba47
bikerJas <18085914+bikerJas@users.noreply.github.com>
2021-11-18 13:16:12 +0000

    Created seperate file for coffee API

--- src/controllers/coffee_request.test.js
+++ src/controllers/coffee_request.test.js
@@ -1,10 +1,10 @@
 const request = require('supertest');
 const app = require('../app');
 
-describe('Test home API endpoint request', () =>{
+describe('Test coffee API endpoint request', () =>{
     test('GET should return correct message', async () => {
-        const res = await request(app).get('/');
+        const res = await request(app).get('/coffeelover');
         expect(res.statusCode).toEqual(200);
-        expect(res.text).toEqual('Welcome to the Drinks API!');
+        expect(res.text).toEqual('I like coffee!');
     });
 });
