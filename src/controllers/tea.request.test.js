691b172bc895f4234422ccb9f9ac08c42cad1833
bikerJas <18085914+bikerJas@users.noreply.github.com>
2021-11-18 19:37:54 +0000

    Added EarlGrey to the tea selection

--- src/controllers/tea.request.test.js
+++ src/controllers/tea.request.test.js
@@ -3,12 +3,26 @@ const app = require("../app");
 
 describe("Test tea API endpoint request", () => {
   test("GET /tea should return correct object", async () => {
-    const res = await request(app).get("/tea").query({ teaName: "Lemon" });
+    const res = await request(app)
+    .get("/tea")
+    .query({ teaName: "Lemon" });
 
     expect(res.statusCode).toEqual(200);
     expect(res.body).toEqual({
       drinkType: "Tea",
       name: "Lemon",
+    });
+  });
+
+  test("GET /tea should return correct object", async () => {
+    const res = await request(app)
+    .get("/tea")
+    .query({ teaName: "EarlGrey" });
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+      drinkType: "Tea",
+      name: "EarlGrey",
     });
   });
 });
